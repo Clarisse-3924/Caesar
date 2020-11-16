2277d204922e3736da50e76d6efc5b0dc67e446c
Clarisse-3924 <cuwizeyimana3924@gmail.com>
2020-11-16 13:39:43 +0200

    modifying app file

--- src/main/java/caesar/App.java
+++ src/main/java/caesar/App.java
@@ -8,17 +8,17 @@ import static caesar.encript.encrypt;
 public class App {
     public static void main(String[] args)
     {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the String for Encryption: ");
         String stringmessage = new String();
-        stringmessage = sc.next();
+        stringmessage = scanner.next();
 
         System.out.println("Enter the key: ");
         String key = new String();
-        key = sc.next();
+        key = scanner.next();
         Integer shiftkey = Integer.parseInt(key);
         System.out.println(encrypt(stringmessage,shiftkey));
         System.out.println(decrypt(encrypt(stringmessage,shiftkey), shiftkey));
-        sc.close();
+        scanner.close();
     }
 }

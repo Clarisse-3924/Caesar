0761973421527863110c2134672fdae4e7b54f07
Clarisse-3924 <cuwizeyimana3924@gmail.com>
2020-11-16 12:06:25 +0200

    doing test files

--- src/test/java/caesar/encriptTest.java
+++ src/test/java/caesar/encriptTest.java
@@ -1,8 +1,27 @@
 package caesar;
 
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.*;
 
 public class encriptTest {
+    @Test
+    public void newRectangle_instantiatesCorrectly() {
+        rectangle testRectangle = new rectangle(2, 4);
+        assertEquals(true, testRectangle instanceof rectangle);
+    }
 
+    @Test
+    public void newRectangle_getsHeight_2() {
+        rectangle testRectangle = new rectangle(2, 4);
+        assertEquals(2, testRectangle.getHeight());
+    }
+
+    @Test
+    public void getWidth_getsRectangleWidth_4() {
+        rectangle testRectangle = new rectangle(2, 4);
+        assertEquals(4, testRectangle.getWidth());
+    }
 
 }

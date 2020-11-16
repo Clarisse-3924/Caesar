144f3881979e1df6c0866fe302f456a4b239e0ee
Clarisse-3924 <cuwizeyimana3924@gmail.com>
2020-11-16 11:52:02 +0200

    deleting wrong test

--- src/test/java/caesar/encriptTest.java
+++ src/test/java/caesar/encriptTest.java
@@ -1,15 +1,8 @@
 package caesar;
 
-import org.junit.Test;
-
-import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.*;
 
 public class encriptTest {
 
-    @Test
-    public void newRectangle_instantiatesCorrectly() {
-        encript testRectangle = new encript(2, 4);
-        assertEquals(true, testRectangle instanceof rectangle);
-    }
+
 }
